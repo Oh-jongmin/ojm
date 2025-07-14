73fb613f439bf91eeecc8a83b532e2e94f6db973
Oh-jongmin <dhwhdals2637@gmail.com>
2025-07-14 20:47:06 +0900

    node spec up

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -20,7 +20,7 @@ module "eks" {
 
   eks_managed_node_groups = {
     pnp-node = {
-      instance_types = ["t3.medium"]
+      instance_types = ["t3.large"]
       desired_size   = 2
       min_size       = 1
       max_size       = 3
